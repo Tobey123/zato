bfa97fa897fcf102b972b5126657e39123582cac
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-07-18 21:31:06 +0200

    GH #1065 - Working on --version --verbose.

--- code/zato-cli-v2/src/zato/cli_v2/__init__.py
+++ code/zato-cli-v2/src/zato/cli_v2/__init__.py
@@ -10,6 +10,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # ################################################################################################################################
 
+def main():
+
+    import sys
+
+    # Anything else than --version is handled by Click
+    if '--version' not in sys.argv:
+        handle_click()
+
+    # To show --version, we can just output it directly, without Click.
+    else:
+
+        # Zato
+        from zato.cli_v2.zato_version import get_version
+
+        # Needed no matter if are --verbose or not
+        zato_version = get_version()
+
+        # This is --version --verbose with all the details
+        if '--verbose' in sys.argv:
+            handle_version_verbose(zato_version)
+        else:
+            handle_version_only(zato_version)
+
+# ################################################################################################################################
+
 def handle_click():
     pass
 
@@ -33,7 +58,7 @@ def handle_version_verbose(zato_version):
     # stdlib
     from os import _exit
     from os.path import abspath, dirname, join as path_join
-    from sys import executable, stdout
+    from sys import executable, stdout, version as sys_version
 
     # Distro
     from distro import LinuxDistribution
@@ -44,34 +69,56 @@ def handle_version_verbose(zato_version):
     # Texttable
     from texttable import Texttable
 
+    # ############################################################################################################################
+
+    # Needed to find out how wide the 'Value' column should be, the default one usually suffices but not with long lines
+    value_length = 60
+
+    # This is where the 'zato' command comes from
+    bin_dir = dirname(executable) # type: str
+
+    # Build a zato_version full with its installation path
+    zato_version = ['* %s' % zato_version]
+    zato_version.append('* %s' % abspath(path_join(bin_dir, '..', '..')))
+    zato_version.append('* %s' % path_join(bin_dir, 'zato'))
+    zato_version = '\n'.join(zato_version)
+
     # Get a distribution object describing the current system ..
     distro = LinuxDistribution()
 
     # .. build a system version object.
-    system_version = '%s %s' % (distro._os_release_info['name'], distro._os_release_info['version'])
+    system_version = '* %s %s' % (distro._os_release_info['name'], distro._os_release_info['version'])
 
     # Get path to source code to be able to check the latest commit and any local changes.
-    bin_dir = dirname(executable)
     git_dir = abspath(path_join(bin_dir, '..', '..'))
+
+    # Details of the Python version used
+    python_details = []
+    python_details.append('* %s ' % sys_version.replace('\n', ''))
+    python_details.append('* %s ' % executable)
+    python_details = '\n'.join(python_details)
 
     with sh.pushd(git_dir):
 
         # Details of the last commit
-        last_commit = sh.git('log', '-1', '--pretty=* %H%n* %ad%n* %s', '--date=iso', _tty_out=False).strip()
+        last_commit = sh.git('log', '-1', '--pretty=* %H%n* %ad%n* %s', '--date=iso', _tty_out=False).strip() # type: str
+        value_length = max(value_length, max(len(elem) for elem in last_commit.splitlines()))
 
         # A list of local updates that have no been committed locally
         local_updates = sh.git('status', '--porcelain').rstrip() or '---'
+        value_length = max(value_length, max(len(elem) for elem in local_updates.splitlines()))
 
         # A list of local commits that have not been pushed
         local_commits = sh.git('cherry', '-v').rstrip() or '---'
+        value_length = max(value_length, max(len(elem) for elem in local_commits.splitlines()))
 
     # Key/value
     len_columns = 2
 
     table = Texttable()
-    table.set_cols_width([17, 60])
+    table.set_cols_width([17, value_length])
     table.set_cols_dtype(['t'] * len_columns)
-    table.set_cols_align(['t'] * len_columns)
+    table.set_cols_align(['c', 't'])
     table.set_cols_valign(['m'] * len_columns)
 
     # Headers
@@ -79,8 +126,10 @@ def handle_version_verbose(zato_version):
 
     # Data rows
     rows += [
-        ['Zato version',      zato_version],
-        ['System version',    system_version],
+        ['Zato',   zato_version],
+        ['System', system_version],
+        ['Python', python_details],
+
         ['Git last commit',   last_commit],
         ['Git local updates', local_updates],
         ['Git local commits', local_commits],
@@ -94,30 +143,5 @@ def handle_version_verbose(zato_version):
     stdout.flush()
 
     _exit(0)
-
-# ################################################################################################################################
-
-def main():
-
-    import sys
-
-    # Anything else than --version is handled by Click
-    if '--version' not in sys.argv:
-        handle_click()
-
-    # To show --version, we can just output it directly, without Click.
-    else:
-
-        # Zato
-        from zato.cli_v2.zato_version import get_version
-
-        # Needed no matter if are --verbose or not
-        zato_version = get_version()
-
-        # This is --version --verbose with all the details
-        if '--verbose' in sys.argv:
-            handle_version_verbose(zato_version)
-        else:
-            handle_version_only(zato_version)
 
 # ################################################################################################################################
